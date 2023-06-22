ba6c74bd51bd6e351956bccf4bbac8346a3ddd45
Tom Sikora <tom.sikora03@gmail.com>
2023-06-22 16:06:40 +0200

    MAJ Country.php

--- Country.php
+++ Country.php
@@ -1,67 +1,135 @@
 <?php
 
-class Country {
+declare(strict_types=1);
 
-    private string $name ;
-    private float $surface ;
-    private int $population ;
+class Country
+{
+    private string $name;
+    private float $surface;
+    private int $population;
 
-
-    public function __construct (string $name="Unknown", float $surface = 0, int $population = 0) {
+    /**
+     * Constructeur de la classe Country.
+     *
+     * @param string $name Le nom du pays (par défaut : "Unknown").
+     * @param float $surface La surface du pays en kilomètres carrés (par défaut : 0).
+     * @param int $population La population du pays (par défaut : 0).
+     */
+    public function __construct(string $name="Unknown", float $surface = 0, int $population = 0)
+    {
         $this->name = $name;
         $this->surface = $surface;
         $this->population = $population;
     }
 
-    public function __destruct(){
+    /**
+     * Destructeur de la classe Country.
+     * Affiche un message de destruction contenant le nom du pays.
+     */
+    public function __destruct()
+    {
         echo "Destruction $this->name\n";
     }
 
-    public function print (  ) : void
+    /**
+     * Affiche les informations du pays.
+     * Inclut le nom, la surface et la population.
+     *
+     * @return void
+     */
+    public function print() : void
     {
-        echo "$this->name\n  surface : $this->surface\n  population : $this->population \n";
+        echo "$this->name\n";
+        echo "Surface : {$this->surface}\n";
+        echo "Population : {$this->population}\n";
     }
 
-    public function getPopulation() : int {
+    /**
+     * Accesseur de la classe Country.
+     *
+     * @return int La valeur de la population du pays. sous forme d'un entier.
+     */
+    public function getPopulation() : int
+    {
         return $this->population;
     }
 
-
-    public function setPopulation(int $population) : void {
+    /**
+     * Modificateur de la classe Country.
+     *
+     * @param int $population La population du pays.
+     */
+    public function setPopulation(int $population) : void
+    {
         $this->population = $population;
     }
 
-
-    public function getSurface() : float{
+    /**
+     * Accesseur de la classe Country.
+     *
+     * @return float La surface du pays.
+     */
+    public function getSurface() : float
+    {
         return $this->surface;
     }
 
-    public function setSurface(float $surface) : void {
+    /**
+     * Modificateur de la classe Country.
+     *
+     * @param float $surface La surface du pays.
+     */
+    public function setSurface(float $surface) : void
+    {
         $this->surface = $surface;
     }
 
-
-    public function getName() : string {
+    /**
+     * Accesseur de la classe Country.
+     *
+     * @return string Le nom du pays.
+     */
+    public function getName() : string
+    {
         return $this->name;
     }
 
-
-    public function setName(string $name) : void {
+    /**
+     * Modificateur de la classe Country.
+     *
+     * @param string $name Le nom du pays.
+     */
+    public function setName(string $name) : void
+    {
         $this->name = $name;
     }
 
-    public function getDensity() : float{
+    /**
+     * Calcule la densité de population du pays.
+     *
+     * @return float La densité de population du pays.
+     */
+    public function getDensity() : float
+    {
         $density = 0;
-        if($this->surface > 0 && $this->population > 0)
+        if (($this->surface > 0) && ($this->population > 0)) {
             $density = $this->population / $this->surface;
+        }
         return $density;
     }
 
-
-    public function isEqual (Country $pays)  : bool {
-        $egaux = false;
-        if( $this->surface==$pays->surface && $this->name==$pays->name && $this->population==$pays->population)
-            $egaux=true;
-        return  $egaux;
+    /**
+     * Vérifie si un pays est égal à un autre pays.
+     *
+     * @param Country $pays Le pays à comparer.
+     * @return bool Vrai si les pays sont égaux, sinon faux.
+     */
+    public function isEqual(Country $pays) : bool
+    {
+        $res = false;
+        if (($this->surface == $pays->getSurface()) && ($this->name == $pays->getName()) && ($this->population == $pays->getPopulation())) {
+            $res = true;
+        }
+        return $res;
     }
 }
